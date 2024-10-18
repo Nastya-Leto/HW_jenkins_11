08bae048cc827c6fb5442a7f3c2c48ba56462248
Nastya-Leto <soidberg91@gmail.com>
2024-10-18 09:49:10 +0400

    Добавление тестов

---
+++ tests/test_fill_form.py
@@ -0,0 +1,44 @@
+from pages.registration_page import RegistrationPage
+import os, allure
+
+
+@allure.tag('web')
+@allure.label("owner", 'zakharovaaa')
+@allure.feature('Форма регистрации студента')
+@allure.story('Заполнения всех полей')
+@allure.title('Заполнить все поля на форме')
+@allure.suite('Проверка формы регистрации')
+def test_fill_form():
+    registration_page = RegistrationPage()
+    current_dir = os.path.dirname(__file__)
+    download_picture = os.path.abspath(os.path.join(current_dir, '..', 'resources', 'image.jpg'))
+
+    with allure.step('Открыть страницу формы'):
+        registration_page.open('/automation-practice-form')
+
+    with allure.step('Заполнить форму'):
+        registration_page.fill_first_name('Анастасия')
+        registration_page.fill_last_name('З')
+        registration_page.fill_user_email('test@mail.ru')
+        registration_page.fill_female()
+        registration_page.fill_user_number('7927000000')
+        registration_page.fill_date_of_birth(20, 6, 1995)
+        registration_page.fill_subjects('Arts')
+        registration_page.fill_hobbies()
+        registration_page.fill_picture(download_picture)
+        registration_page.fill_current_address('Samara')
+        registration_page.fill_state_and_city()
+        registration_page.submit_button()
+
+    with allure.step('Проверить корректность заполнения формы'):
+        registration_page.should_registered_user_with(
+            'Анастасия З',
+            'test@mail.ru',
+            'Female',
+            '7927000000',
+            '20 July,1995',
+            'Arts',
+            'Music',
+            'image.jpg',
+            'Samara',
+            'Haryana Karnal')
